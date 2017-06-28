b45f0d34ff4ed47567580291a20aa4be0cd6141f
Dave Justice <djustice@mozilla.com>
2017-06-28 16:50:20 -0400

    replace testpilot-metric with ga calls

--- webextension/lib/send-metrics-data.js
+++ webextension/lib/send-metrics-data.js
@@ -1,33 +1,7 @@
-const Metrics = require('testpilot-metrics');
 const storage = browser.storage.local;
 const manifest = require('../package.json');
 
-const encoded = this._formEncode(msg);
 const GA_URL = 'https://ssl.google-analytics.com/collect';
-
-if ('sendBeacon' in navigator) {
-  navigator.sendBeacon(url, formEncodedData);
-} else {
-  const config = {
-    method: 'POST',
-    mode: 'cors',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
-    },
-    body: formEncodedData
-  };
-  fetch(url, config)
-    .then((resp) => console.log(`Sent GA message via fetch: ${formEncodedData}`))
-    .catch((err) => console.error(`GA sending via fetch failed: ${err}`));
-}
-
-const { sendEvent } = new Metrics({
-  id: require('sdk/self').id,
-  type: 'sdk',
-  version: manifest.version,
-  uid: storage.clientUUID,
-  tid: manifest.config['GA_TRACKING_ID']
-});
 
 module.exports = sendMetricsData;
 
@@ -41,20 +15,38 @@ function sendMetricsData(o, win) {
 
   // NOTE: this packet follows a predefined data format and cannot be changed
   //       without notifying the data team. See docs/metrics.md for more.
-  sendEvent({
-    object: o.object,
-    method: o.method,
-    domain: o.domain,
-    video_x: coords.left,
-    video_y: coords.top,
-    video_width: coords.width,
-    video_height: coords.height
-  }, function transform(input, output) {
-    output.cd2 = input['video_x'];
-    output.cd3 = input['video_y'];
-    output.cd4 = input['video_width'];
-    output.cd5 = input['video_height'];
-    output.cd6 = input.domain;
-    return output;
-  });
+  const formEncodedData = Object.keys({
+    v: 1,
+    aip: 1, // anonymize user IP addresses (#24 mozilla/testpilot-metrics)
+    an: browser.runtime.id,
+    av: manifest.version,
+    tid: manifest.config['GA_TRACKING_ID'],
+    cid: storage.clientUUID,
+    t: 'event',
+    ec: o.category,
+    ea: o.method,
+    cd2: coords.left, // video_x
+    cd3: coords.top, // video_y
+    cd4: coords.width,
+    cd5: coords.height,
+    cd6: o.domain,
+    el: o.object
+  }).map(item => encodeURIComponent(item) + '=' + encodeURIComponent(obj[item]))
+    .join('&');
+
+  if ('sendBeacon' in navigator) {
+    navigator.sendBeacon(GA_URL, formEncodedData);
+  } else {
+    const config = {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
+      },
+      body: formEncodedData
+    };
+    fetch(GA_URL, config)
+      .then(() => console.log(`Sent GA message via fetch: ${formEncodedData}`))
+      .catch((err) => console.error(`GA sending via fetch failed: ${err}`));
+  }
 }
