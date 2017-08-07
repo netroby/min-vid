a5011ab2071492f98a4d1a0e9b2546c0be33e4cf
Dave Justice <djustice@mozilla.com>
2017-08-07 12:42:51 -0400

    Address issues with options page (dimensions setting)
    - fixes #945
    - fixes #953

--- webextension/options/options.js
+++ webextension/options/options.js
@@ -1,27 +1,7 @@
-function setMax() {
-  let width = 0;
-  let height = 0;
-
-  if (typeof(window.innerWidth) === 'number') {
-    height = window.innerHeight;
-    width = window.innerWidth;
-  } else if (document.documentElement && (document.documentElement.clientWidth || document.documentElement.clientHeight)) {
-    height = document.documentElement.clientHeight;
-    width = document.documentElement.clientWidth;
-  } else if (document.body && (document.body.clientWidth || document.body.clientHeight)) {
-    height = document.body.clientHeight;
-    width = document.body.clientWidth;
-  }
-
-  document.querySelector('#min-vid-width').setAttribute('max', width);
-  document.querySelector('#min-vid-height').setAttribute('max', height);
-}
-
-document.addEventListener('DOMContentLoaded', setMax);
-document.querySelector('form').addEventListener('submit', function() {
-  browser.storage.set({
+document.querySelector('form').addEventListener('submit', function(ev) {
+  ev.preventDefault();
+  browser.storage.local.set({
     width: document.querySelector('#min-vid-width').value,
     height: document.querySelector('#min-vid-height').value
   });
-  e.preventDefault();
 });
